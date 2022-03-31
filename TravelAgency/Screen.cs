6a029552230f381442f2e7fbf2ac0d98d0e2fd6b
Patryk50 <patryk.kordel@gmail.com>
2022-03-31 23:13:40 +0200

    31.03.2022 v2

--- TravelAgency/Screen.cs
+++ TravelAgency/Screen.cs
@@ -11,8 +11,6 @@ namespace TravelAgency
     {
         public static void InitialScreen()
         {
-            Office office = new Office();
-
             Console.WriteLine("DZISIEJSZE PROMOWANE OFERTY");
             Console.WriteLine("--------------------------------");
             Template(1);
@@ -22,14 +20,35 @@ namespace TravelAgency
         static void Template(int number)
         {
             Office office = new Office();
-
             foreach (var hotel in office.SelectedHotels)
             {
+                string feeding;
+                //Instrukcja warunkowa odpowiadająca za wyświetlenie rodzaju wyżywienia
+                if (number == 1)
+                {
+                    feeding = "śniadanie";
+                }
+                else
+                {
+                    feeding = "All Inclusive";
+                }
+
+                //Instrukcja warunkowa odpowiadająca za przedstawienie innej daty powrotu i sumy spędzonych dni
+                if (number == 2)
+                {
+                    office.ArrivalDate = DateTime.Now.AddDays(10);
+                    office.Difference = (office.ArrivalDate - office.DepartureDate).Days;
+                }
+                else if (number == 3)
+                {
+                    office.ArrivalDate = DateTime.Now.AddDays(14);
+                    office.Difference = (office.ArrivalDate - office.DepartureDate).Days;
+                }
                 Console.WriteLine($"NUMER: {number++}");
                 Console.WriteLine($"KRAJ: {hotel.Country}");
                 Console.WriteLine($"TERMIN: {office.DepartureDate.ToShortDateString()} - {office.ArrivalDate.ToShortDateString()} ({office.Difference} dni)");
                 Console.WriteLine($"HOTEL: {hotel.HotelName} {hotel.Category}");
-                Console.WriteLine("WYŻYWIENIE: śniadanie");
+                Console.WriteLine($"WYŻYWIENIE: {feeding}");
                 Console.WriteLine($"CENA: {hotel.Price} PLN/os");
                 Console.WriteLine("--------------------------------");
             }
